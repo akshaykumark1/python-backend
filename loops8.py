41bc96474104aed2c5e5ce189ec046b32fa684dc
akshaykumark1 <kumarakshay90574@gmail.com>
2024-10-15 09:02:10 +0530

    done

--- loops8.py
+++ loops8.py
@@ -3,6 +3,9 @@
 # ***
 # ***
 
+# this will print 4 rows of 4 columns of asterisks(*) 
+# the outer loop will run 4 times and the inner loop will run 4 times for each outer loop iteration
+# thus it will print 4 rows of 4 columns of asterisks(*)
 for i in range(4):
     for j in range(4):
         print("*",end=" ")
@@ -44,17 +47,39 @@ for i in range(1,4):
 # **
 # ***
 # ****
+# how it works
+# outer loop will run 4 times
+# inner loop will run i times
+# it will print * i times
+# and then it will go to next line
+# so it will print 1, 2, 3, 4 stars
+# and so on
+# so we get the pattern
 for i in range(1,5):
     for j in range(1,i+1):
         print("*",end=" ")
     print()
 
-
 # print this pattern
 #   ***
 #   **
 #   *
-for i in range(4,0,-1):
+# this will print the pattern
+#   ***
+#   **
+#   *
+# how it works
+# outer loop will run 10 times
+# inner loop will run i times
+# it will print * i times
+# and then it will go to next line
+# so it will print 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 stars
+# and then it will go to next line
+# and then it will print 9, 8, 7, 6, 5, 4, 3, 2, 1 stars
+# and then it will go to next line
+# and so on
+# so we get the pattern
+for i in range(10,0,-1):
     for j in range(i):
         print("*",end=" ")
     print()
---
+++ string.py
@@ -0,0 +1,8 @@
+# print string reverse
+a=input("enter the word")
+reverse=""
+for i in a:
+    reverse=i+reverse
+print(reverse)
+
+
