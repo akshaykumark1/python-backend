5952a74501493093457e16cf5db38dcb96790f23
akshaykumark1 <kumarakshay90574@gmail.com>
2024-11-20 22:02:26 +0530

    done

--- REGEX/work2.py
+++ REGEX/work2.py
@@ -1,7 +1,7 @@
 # password validation
 import re
 password=input("enter the password")
-x="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,}$"
+x="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,}$"
 if re.search(x,password):
     print("valid password")
 else:
